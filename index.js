7da4d724205dbe820c0c43b78c52f42ba3fed20b
Héctor Luis Fernández <Hectorfernandez@live.com>
2022-06-28 15:16:30 -0400

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: "Sample task #1",
+      isCompleted: false,
+    },
+    {
+      text: "Sample task #2",
+      isCompleted: false,
+    },
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text, isCompleted: false }];
+    setTodos(newTodos);
+  };
+  const removeTodo = index => {
+    let temp = [...todos];
+    temp.splice(index, 1);
+    setTodos(temp);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
